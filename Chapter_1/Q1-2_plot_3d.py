40c04ef384b2069ad586882e5f7998489c7a76b2
kcyoon689 <kcyoun689@gmail.com>
2023-07-13 18:44:12 +0900

    edit question2

--- Chapter_1/Q1-2_plot_3d.py
+++ Chapter_1/Q1-2_plot_3d.py
@@ -1,20 +1,6 @@
 # Plot the following training set in three-dimensional space and whether it is linearly separable and why.
 # 다음 훈련 집합을 3차원 공간에 플롯하고 선형 분리 가능한지 여부와 그 이유를 설명합니다.
-'''
-\begin{align}
-x_1 = \begin{pmatrix} 0 \\ 0 \\ 0 \end{pmatrix} 
-,x_2 = \begin{pmatrix} 0 \\ 1 \\ 1 \end{pmatrix} 
-,x_3 = \begin{pmatrix} 1 \\ 1 \\ 1 \end{pmatrix} 
-,x_4 = \begin{pmatrix} 1 \\ 0 \\ 1 \end{pmatrix} 
-,x_5 = \begin{pmatrix} 0 \\ 0 \\ 1 \end{pmatrix} 
-,x_6 = \begin{pmatrix} 0 \\ 1 \\ 0 \end{pmatrix} 
-\end{align}
 
-\begin{align}
-y_1 = 1, y_2 = 1, y_3 = 1, y_4 = -1, y_5 = -1, y_6 = -1 
-\end{align}
-
-'''
 import plotly.graph_objects as go
 
 # below is 1
